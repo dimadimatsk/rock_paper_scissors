8ae695c97077d73ff6dab9188cdf243333e37006
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2021-12-01 13:28:33 +0700

    first version of game

---
+++ scripts/game.js
@@ -0,0 +1,78 @@
+const gameValues = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+  let randomValue = gameValues[Math.floor(Math.random() * gameValues.length)];
+  return randomValue;
+}
+
+function foolCheck() {
+  playerSelection = prompt("Choice your weapon (Rock, Paper or Scissors)!");
+  playerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+  while (gameValues.includes(playerSelection) === false) {
+    alert(
+      "Wrong weapon. Choice from available items (Rock, Paper or Scissors)!"
+    );
+    playerSelection = prompt("Choice your weapon (Rock, Paper or Scissors)!");
+    playerSelection =
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+  }
+  return playerSelection;
+}
+
+function theGame() {
+  playerSelection = foolCheck();
+  computerSelection = computerPlay();
+  if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return 0;
+  } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return 0;
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return 0;
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 2;
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 2;
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 2;
+  } else {
+    alert(`It's a draw!`);
+    return 1;
+  }
+}
+
+let yourScore = 0;
+let compScore = 0;
+
+for (let i = 0; i < 5; i++) {
+  switch (theGame()) {
+    case 2:
+      yourScore += 1;
+      break;
+    case 0:
+      compScore += 1;
+      break;
+    default:
+      yourScore += 1;
+      compScore += 1;
+      break;
+  }
+}
+
+function scoreAnouncer() {
+    if (yourScore > compScore) {
+        alert(`You win! ${yourScore} - ${compScore}`);
+    } else {
+        alert(`You lose! ${yourScore} - ${compScore}`);
+    }
+};
+
+scoreAnouncer()
+
+

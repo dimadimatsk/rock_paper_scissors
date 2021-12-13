fe8557dd3f89ac710917c0a5e1fd06c58eb2ea4b
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2021-12-13 22:37:39 +0700

    r p s game; version without endscore

--- scripts/game.js
+++ scripts/game.js
@@ -1,76 +1,109 @@
-const gameValues = ["Rock", "Paper", "Scissors"];
+// const weapons
+const gameValues = ["rock", "paper", "scissors"];
 
+// game scores
+let playerScore = 0;
+let compScore = 0;
+let roundWinner = "";
+
+// comp weapon choice
 function computerPlay() {
   let randomValue = gameValues[Math.floor(Math.random() * gameValues.length)];
   return randomValue;
 }
 
-function foolCheck() {
-  playerSelection = prompt("Choice your weapon (Rock, Paper or Scissors)!");
-  playerSelection =
-    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  while (gameValues.includes(playerSelection) === false) {
-    alert(
-      "Wrong weapon. Choice from available items (Rock, Paper or Scissors)!"
-    );
-    playerSelection = prompt("Choice your weapon (Rock, Paper or Scissors)!");
-    playerSelection =
-      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  }
-  return playerSelection;
-}
-
-function playGame() {
-  playerSelection = foolCheck();
+// game function
+function playGame(playerSelection) {
   computerSelection = computerPlay();
-  if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return 0;
-  } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return 0;
-  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return 0;
-  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-    return 2;
-  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-    return 2;
-  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-    return 2;
+  if (
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    compScore += 1;
+    roundWinner = "computer";
+  }
+  if (
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    playerScore += 1;
+    roundWinner = "player";
+  }
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie";
+  }
+}
+
+// gameover check
+function isGameOver() {
+  if (playerScore === 5 || compScore === 5) {
+    return true;
+  }
+  return false;
+}
+
+// play game
+
+function memChoice(e) {
+  playerSelection = e.target.classList[e.target.classList.length - 1];
+  if (isGameOver === true) {
+    return;
+  }
+  playGame(playerSelection);
+  updateScore();
+  changeScoreMsg();
+}
+
+// update score
+
+function updateScore() {
+  if (roundWinner === "tie") {
+    scoreInfo.textContent = "It's a tie!";
+  } else if (roundWinner === "player") {
+    scoreInfo.textContent = "You won!";
   } else {
-    alert(`It's a draw!`);
-    return 1;
+    scoreInfo.textContent = "You lost!";
   }
+  playerScoreValue.textContent = `Player: ${playerScore}`;
+  computerScoreValue.textContent = `Enemy: ${compScore}`;
 }
 
-let yourScore = 0;
-let compScore = 0;
-
-while (compScore < 5 && yourScore < 5) {
-  switch (playGame()) {
-    case 2:
-      yourScore += 1;
-      break;
-    case 0:
-      compScore += 1;
-      break;
-    default:
-      yourScore += 0;
-      compScore += 0;
-      break;
+// change score msg
+function changeScoreMsg() {
+  if (roundWinner === "player") {
+    scoreMsg.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} crushes ${computerSelection}`;
   }
+  if (roundWinner === "computer") {
+    scoreMsg.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} is owned by ${computerSelection}`;
+  }
+  if (roundWinner === "tie") {
+    scoreMsg.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} ties with ${computerSelection}`;
+  };
+};
+
+// capitalize first letter
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-function scoreAnounce() {
-  if (yourScore > compScore) {
-    alert(`You win! ${yourScore} - ${compScore}`);
-  } else {
-    alert(`You lose! ${yourScore} - ${compScore}`);
-  }
-}
+// consts for change DOM
+const scoreInfo = document.getElementById("scoreInfo");
+const scoreMsg = document.getElementById("scoreMsg");
+const playerScoreValue = document.getElementById("playerScore");
+const computerScoreValue = document.getElementById("compScore");
 
-scoreAnounce();
+// choice all buttons from page
+const buttons = document.querySelectorAll(".btn");
+
+// player choices rock, paper or scissors for fight; activate game;
+buttons.forEach((btn) => {
+  btn.addEventListener("click", memChoice);
+});
